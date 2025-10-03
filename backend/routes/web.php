0d076f7354a32c6d0b97a4415011789d63cb93ba
t-aoto-eses <t.aoto@eses-inc.jp>
2025-10-03 15:17:06 +0900

    コントローラーからレスポンスを返せるかを確認した。

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('api/test', function () {
+Route::get('/api/test', function () {
     return response()->json(['message' => 'Laravelから送信したAPIです']);
 });
+
+Route::get('api/test2', [ApiController::class, 'test']);
